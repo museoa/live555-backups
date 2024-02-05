40d33a8ed17ec0ccc1830b72aecfca9524b951ff
Anderson Torres <torres.anderson.85@protonmail.com>
2024-02-05 13:51:39 -0300

    [backup] LIVE555 2020.01.10 [retrived from https://src.rrz.uni-hamburg.de/files/src/live555/]

--- live/BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
+++ live/BasicUsageEnvironment/include/BasicUsageEnvironment_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "BasicUsageEnvironment" library
-// Copyright (c) 1996-2019 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2020 Live Networks, Inc.  All rights reserved.
 
 #ifndef _BASICUSAGEENVIRONMENT_VERSION_HH
 #define _BASICUSAGEENVIRONMENT_VERSION_HH
 
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2019.12.13"
-#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1576195200
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2020.01.10"
+#define BASICUSAGEENVIRONMENT_LIBRARY_VERSION_INT		1578614400
 
 #endif
--- live/groupsock/include/groupsock_version.hh
+++ live/groupsock/include/groupsock_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "groupsock" library
-// Copyright (c) 1996-2019 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2020 Live Networks, Inc.  All rights reserved.
 
 #ifndef _GROUPSOCK_VERSION_HH
 #define _GROUPSOCK_VERSION_HH
 
-#define GROUPSOCK_LIBRARY_VERSION_STRING	"2019.12.13"
-#define GROUPSOCK_LIBRARY_VERSION_INT		1576195200
+#define GROUPSOCK_LIBRARY_VERSION_STRING	"2020.01.10"
+#define GROUPSOCK_LIBRARY_VERSION_INT		1578614400
 
 #endif
