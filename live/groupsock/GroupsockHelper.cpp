fd42e1d3bb63ea31d013f74a5bc3a8c04963e40a
Anderson Torres <torres.anderson.85@protonmail.com>
2024-02-05 13:51:37 -0300

    [backup] LIVE555 2018.08.27 [retrived from https://src.rrz.uni-hamburg.de/files/src/live555/]

--- live/groupsock/GroupsockHelper.cpp
+++ live/groupsock/GroupsockHelper.cpp
@@ -229,10 +229,12 @@ Boolean setSocketKeepAlive(int sock) {
     return False;
   }
 
+#ifdef TCP_KEEPIDLE
   int const keepalive_time = 180;
   if (setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, (void*)&keepalive_time, sizeof keepalive_time) < 0) {
     return False;
   }
+#endif
 
   int const keepalive_count = 5;
   if (setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, (void*)&keepalive_count, sizeof keepalive_count) < 0) {
