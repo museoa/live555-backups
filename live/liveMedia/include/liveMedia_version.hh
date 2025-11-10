803bac615d8172734fed65177a3e265683efa9f5
Anderson Torres <anderson.torres.8519@gmail.com>
2025-11-10 00:31:09 -0300

    [backup] LIVE555 2025.07.10 [retrived from https://src.rrz.uni-hamburg.de/files/src/live555/]

--- live/liveMedia/include/liveMedia_version.hh
+++ live/liveMedia/include/liveMedia_version.hh
@@ -19,8 +19,8 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 #ifndef _LIVEMEDIA_VERSION_HH
 #define _LIVEMEDIA_VERSION_HH
 
-#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2025.05.08"
-#define LIVEMEDIA_LIBRARY_VERSION_INT		1746662400
+#define LIVEMEDIA_LIBRARY_VERSION_STRING	"2025.07.10"
+#define LIVEMEDIA_LIBRARY_VERSION_INT		1752105600
 
 extern char const* const liveMediaLibraryVersionStr;
 extern int const liveMediaLibraryVersionInt;
